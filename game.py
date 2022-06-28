3dd7cd482ac43620d56bd16065144662cba09129
Wallison <wallisonjose22@outlook.com>
2022-06-27 23:55:10 -0300

    Criação da linha limite de até onde os invasores porem ir + Criação de naves invasoras em posições aleatórias

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ from pygame.locals import *
 
 from sys import exit
 
+from random import randint 
+
 pygame.init()
 
 # Largura e altura da tela 
@@ -12,12 +14,21 @@ altura = 480
 
 # Coordenadas de acordo com as dimensões da tela 
 # Com a nave iniciando nesse local, ela irá aparecer no meio da tela 
-x = largura / 2
+x = ( largura / 2 ) - 40
 y = 380
+
+# Posições em que as naves invasoras irão aparecer no game
+x_invasor1 = randint(0, 560)
+y_invasor1 = 1
+
+x_invasor2 = randint(0, 560)
+y_invasor2 = 1
 
 # Imagens do game 
 fundo = pygame.image.load('img/fundo.png')
 nave_u = pygame.image.load('img/nave_usuario.png')
+invasor_1 = pygame.image.load('img/invasor-1.png')
+invasor_2 = pygame.image.load('img/invasor-2.png')
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Space Defender')
@@ -37,7 +48,7 @@ while True:
     # Comandos para movimentar a nave pela tela 
 
     if pygame.key.get_pressed()[K_RIGHT]:
-        if x < 600:
+        if x < 560:
             x = x + 10
     
     if pygame.key.get_pressed()[K_LEFT]:
@@ -46,6 +57,11 @@ while True:
 
     
     nave_usuario = tela.blit(nave_u, (x, y))
+
+    limite_nave = pygame.draw.line(tela, (0, 0, 0), (0, 395), (640, 395), 1)
+
+    nave_inv1 = tela.blit(invasor_1, (x_invasor1, y_invasor1))
+    nave_inv2 = tela.blit(invasor_2, (x_invasor2, y_invasor2))
 
     
     pygame.display.update()
